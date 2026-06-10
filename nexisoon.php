18b566a4b03e39f386466d533504edb9a97fd2da
hmbashar <hmbashar@gmail.com>
2026-06-10 18:54:32 +0600

    update readme

--- nexisoon.php
+++ nexisoon.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: NexiSoon – Coming Soon, Under Construction & Maintenance Mode
- * Plugin URI: https://nexiby.com/nexisoon
+ * Plugin URI: https://github.com/devatiq/nexisoon
  * Description: Create a beautiful coming soon, under construction, or maintenance mode page for your WordPress website.
  * Version: 1.0.0
  * Author: Nexiby LLC
@@ -16,13 +16,13 @@
  * @package NexiSoon
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-define( 'NEXISOON_VERSION', '1.0.0' );
-define( 'NEXISOON_PLUGIN_FILE', __FILE__ );
-define( 'NEXISOON_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'NEXISOON_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'NEXISOON_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define('NEXISOON_VERSION', '1.0.0');
+define('NEXISOON_PLUGIN_FILE', __FILE__);
+define('NEXISOON_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('NEXISOON_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('NEXISOON_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
 require_once NEXISOON_PLUGIN_DIR . 'includes/helpers.php';
 require_once NEXISOON_PLUGIN_DIR . 'includes/class-nexisoon-settings.php';
@@ -31,14 +31,15 @@ require_once NEXISOON_PLUGIN_DIR . 'includes/class-nexisoon-admin.php';
 require_once NEXISOON_PLUGIN_DIR . 'includes/class-nexisoon-frontend.php';
 require_once NEXISOON_PLUGIN_DIR . 'includes/class-nexisoon.php';
 
-register_activation_hook( __FILE__, array( 'NexiSoon', 'activate' ) );
+register_activation_hook(__FILE__, array('NexiSoon', 'activate'));
 
 /**
  * Load the plugin.
  *
  * @return void
  */
-function nexisoon_init() {
+function nexisoon_init()
+{
 	NexiSoon::instance();
 }
-add_action( 'plugins_loaded', 'nexisoon_init' );
+add_action('plugins_loaded', 'nexisoon_init');
